2c0ec033ddeb0ef581f0102a7eb32b539f6feb08
austinp0wers <austintaetious@gmail.com>
2022-12-13 09:22:50 +0900

    ADDED: basic code structures for authentication

---
+++ src/shared/services/api-config.service.ts
@@ -0,0 +1,82 @@
+import { SnakeNamingStrategy } from './../snake-naming.strategy';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+import { isNil } from 'lodash';
+
+@Injectable()
+export class ApiConfigService {
+  constructor(private configService: ConfigService) {}
+
+  get isDevelopment(): boolean {
+    return this.nodeEnv === 'development';
+  }
+
+  get isProduction(): boolean {
+    return this.nodeEnv === 'production';
+  }
+
+  get isTest(): boolean {
+    return this.nodeEnv === 'test';
+  }
+
+  private getNumber(key: string): number {
+    const value = this.get(key);
+
+    try {
+      return Number(value);
+    } catch {
+      throw new Error(key + ' environment variable is not a number');
+    }
+  }
+
+  get nodeEnv(): string {
+    return this.getString('NODE_ENV');
+  }
+
+  get authConfig() {
+    return {
+      privateKey: this.getString('JWT_PRIVATE_KEY'),
+      publicKey: this.getString('JWT_PUBLIC_KEY'),
+      jwtExpirationTime: this.getNumber('JWT_EXPIRATION_TIME'),
+    };
+  }
+
+  private getString(key: string): string {
+    const value = this.get(key);
+
+    return value.replace(/\\n/g, '\n');
+  }
+
+  get postgresConfig(): TypeOrmModuleOptions {
+    const entities = [
+      __dirname + '/../../modules/**/*.entity{.ts,.js}',
+      __dirname + '/../../modules/**/*.view-entity{.ts,.js}',
+    ];
+
+    return {
+      entities,
+      keepConnectionAlive: !this.isTest,
+      dropSchema: this.isTest,
+      type: 'postgres',
+      name: 'default',
+      host: this.getString('DB_HOST'),
+      port: this.getNumber('DB_PORT'),
+      username: this.getString('DB_USERNAME'),
+      password: this.getString('DB_PASSWORD'),
+      database: this.getString('DB_DATABASE'),
+      migrationsRun: true,
+      namingStrategy: new SnakeNamingStrategy(),
+    };
+  }
+  private get(key: string): string {
+    const value = this.configService.get<string>(key);
+    console.log('value', value);
+    if (isNil(value)) {
+      throw new Error(key + ' environment variable does not set'); // probably we should call process.exit() too to avoid locking the service
+    }
+
+    return value;
+  }
+}
